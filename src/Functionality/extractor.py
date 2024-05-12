df2a0d5cdc3aeb547e4c38fb48fc5afa298b4006
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-05-13 00:11:57 +0530

    proxy changed

--- src/Functionality/extractor.py
+++ src/Functionality/extractor.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore")
 
 def get_data(team):
     df = pd.DataFrame()
-    gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable = team_list.get(team),proxy='152.170.208.188:8080')  
+    gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable = team_list.get(team),proxy='188.74.183.10:8279')  
     games = gamefinder.get_data_frames()[0].head()
     games['HOME'] = 0
     games['AWAY'] = 0
